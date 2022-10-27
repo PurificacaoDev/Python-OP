0b99bf9def428fea4ed19baf6da3f8e2a75455ea
Douglas Purificação <purificacaodev@gmail.com>
2022-10-26 22:39:24 -0300

    Aula 1 26.10.22

---
+++ teste.py
@@ -0,0 +1,13 @@
+pais = "Italia"
+quantidade = 15
+dia = 15
+mes = 10
+ano = 2015
+type(pais)
+type(quantidade)
+print(dia,mes,ano, sep="/")
+print(pais," ganhou", quantidade,"titulos mundiais")
+print("Brasil ganhou 5 titulos mundiais", sep=" ")
+print("Brasil","ganhou",5,"titulos","mundiais", sep=" - ")
+print("Brasil","ganhou",5,"titulos mundiais",end="\n")
+print(" ")
