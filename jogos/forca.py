e8022389c261c12bae0cf3e9c6cd97df202fabb1
Douglas Purificação <purificacaodev@gmail.com>
2022-11-03 22:00:31 -0300

    Aula do dia 03.11.22

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,11 +1,24 @@
-
+import os
 def jogo_forca():
     print("********************************")
     print("Bem vindo ao jogo de Forca")
     print("********************************\n")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.lower() == letra.lower()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
     print("**************")
     print("Fim de Game!!")
+    print("\n" * os.get_terminal_size().lines)
     print("**************")
 
 if(__name__ == "__main__"):
