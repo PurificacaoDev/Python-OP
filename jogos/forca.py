b57fb3535f055c9fa42cf291d399701e090542e0
Douglas Purificação <purificacaodev@gmail.com>
2022-11-10 22:27:52 -0300

    Aula do dia 10.11.22

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,41 +1,77 @@
-def jogo_forca():
-    print("********************************")
-    print("Bem vindo ao jogo de Forca")
-    print("********************************\n")
+import random
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_" for letra in palavra_secreta]
+def jogo_forca():
+    tela_inicial()
+
+    palavra_secreta = gerar_palavra_secreta()
+    letras_acertadas = inicializa_letras(palavra_secreta)
     palavra_listada = list(palavra_secreta)
     enforcou = False
     acertou = False
     erros = 0
+    print(palavra_secreta)
 
     while(not enforcou and not acertou):
-        chute = input("Qual letra? ")
-        chute = chute.strip()
-        index = 0
+        chute = pegar_chute()
         if(chute.lower() in palavra_secreta):
-            for letra in palavra_secreta:
-                if(chute.lower() == letra.lower()):
-                    letras_acertadas[index] = letra
-                    #print("Encontrei a letra {} na posição {}".format(letra, index))
-                index += 1
+            marca_chute(chute, letras_acertadas, palavra_secreta)
             if(palavra_listada == letras_acertadas):
                 print("A palavra correta é: ",palavra_secreta)
                 mensagem_vitoria()
                 break
         else:
             erros += 1
-        print(letras_acertadas)
-        print("Voce errou {} de {}".format(erros,len(palavra_secreta)))
-
-        if(erros == 6):
-            enforcou = erros == 6
+            #print("Voce errou {} de {}".format(erros,len(palavra_secreta)))
+            desenha_erros(erros)
+        if(erros == 7):
+            enforcou = erros == 7
             mensagem_derrota()
+        print(letras_acertadas)
 
     print("**************")
     print("Fim de Game!!")
     print("**************")
+
+#Função utilizada para marcar os chutes corretos
+def marca_chute(chute,letras_acertadas,palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute.lower() == letra.lower()):
+            letras_acertadas[index] = letra
+        index += 1
+
+#Inicia as palavras com o _ no lugar das palavras
+def inicializa_letras(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
+
+#Tela de inicio do jogo
+def tela_inicial():
+    print("********************************")
+    print("Bem vindo ao jogo de Forca")
+    print("********************************\n")
+
+#Função definada para pegar o chute do operador
+def pegar_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip()
+    return chute
+
+#Aqui fazemos a escolha da palavra secreta que sera gerar aleatoriamente dentro do arquivo TXT do projeto
+def gerar_palavra_secreta ():
+
+    palavras = []
+    arquivo = open("palavras.txt","r", encoding="utf-8")
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero]
+    return palavra_secreta
+
+#Mensagem de vitoria com o Giga Chad
 def mensagem_vitoria():
     print("⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿")
     print("⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿ ")
@@ -62,6 +98,8 @@ def mensagem_vitoria():
     print("⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿")
     print("⣿⣿⣿⣿⠿⠛⠉⠉⠁⠀⢻⣿⡇⠀⠀⠀⠀⠀⠀⢀⠈⣿⣿⡿⠉⠛⠛⠛⠉⠉")
     print("⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄")
+
+#Mensagem de derrota
 def mensagem_derrota():
 
     print("⠀⠀⠀⠀⠀⠀⠀ ⠀⢀⡠⠔⠒⠒⠒⠒⠒⠢⠤⣀⠀")
@@ -79,12 +117,57 @@ def mensagem_derrota():
     print("⠀⠀⠀⠀⣀⣠⠊⠀⣀⠎⠁⠀⠀⠀⠙⠳⢴⡦⡴⢶⣞⣁⣀⣀⡀⠀")
     print("⠀⠐⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⢀⠤⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⠀")
 
+#Desenha a forca com o boneco
+def desenha_erros(erros):
+    print("  _______     ")
+    print(" |/      |    ")
 
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
 
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+#Executa o game quando ele é chamado dentro do proprio arquivo
 if(__name__ == "__main__"):
     jogo_forca()
-
-
-
-
-## len(Array com a palavra banana) pra encontrar o tamanho do mesmo
