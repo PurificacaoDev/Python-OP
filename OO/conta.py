3b29b4e0fcbec61ffb1dac1d7d748db81f7e29fe
Douglas Purificação <purificacaodev@gmail.com>
2022-11-12 18:50:40 -0300

    Concluindo os metodos e aplicando em um pequeno desafio

--- OO/conta.py
+++ OO/conta.py
@@ -1,10 +1,23 @@
 
 
 class Conta:
-
+    #Cria uma conta com seus atributos
     def __init__(self,numero, titular, saldo, limite):
-        print("Construtor")
+        print("Construindo o Objeto")
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    #Imprimi o saldo atual da conta
+    def extrato(self):
+        print("{} seu saldo é de R${}".format(self.titular,self.saldo))
+
+    #Efetua um depoisito na conta criada anteriormente
+    def depositar(self,valor):
+        self.saldo += valor
+
+    #Saca um valor na conta criada anteriormente
+    def sacar(self,valor):
+        self.saldo -= valor
+        
