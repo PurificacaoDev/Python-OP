3d63468f6c9b8cbb4510339baea9b3ba35ab9c26
Douglas Purificação <purificacaodev@gmail.com>
2022-11-12 17:41:08 -0300

    Aula do dia 12.11.22

---
+++ OO/teste.py
@@ -0,0 +1,14 @@
+
+
+def criar_conta(numero,titular,saldo,limite):
+    conta = {"numero":numero,"titular":titular,"saldo":saldo,"limite":limite}
+    return conta
+def depositar(conta,valor):
+    conta["saldo"] += valor
+
+def sacar(conta,valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Seu saldo atual é de {}".format(conta["saldo"]))
+
