1686d8489406691f876103c8f350161dfe4edda7
Douglas Purificação <purificacaodev@gmail.com>
2022-11-16 21:58:22 -0300

    AULA DIA 17.11.22

--- OO/conta.py
+++ OO/conta.py
@@ -4,20 +4,37 @@ class Conta:
     #Cria uma conta com seus atributos
     def __init__(self,numero, titular, saldo, limite):
         print("Construindo o Objeto")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     #Imprimi o saldo atual da conta
     def extrato(self):
-        print("{} seu saldo é de R${}".format(self.titular,self.saldo))
+        print("{} seu saldo é de R${}".format(self.__titular,self.saldo))
 
     #Efetua um depoisito na conta criada anteriormente
     def depositar(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     #Saca um valor na conta criada anteriormente
     def sacar(self,valor):
-        self.saldo -= valor
-        
+        self.__saldo -= valor
+
+    def transfere(self,valor,destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+    #Get sempre pega e e "imprime" o valor
+    @property
+    def limite(self):
+        return self.__limite
+    #Set sempre pega e altera o valor ou o atributo
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
